837eebf732bd9a4dbb35660386f10af2e8cd366b
jfling13 <jfling13@gmail.com>
2023-09-13 16:37:15 -0400

    login 路由

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -6,6 +6,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('index/',views.index,name='index'),
+    path('login/',views.login,name='login'),
     path('detail/<int:post_id>',views.detail,name='detail'),
     path('comment/<int:post_id>',views.get_comments_by_post,name='get_comments_by_post'),
     path('add_comment/<int:post_id>',views.add_comment,name='add_comment'),
