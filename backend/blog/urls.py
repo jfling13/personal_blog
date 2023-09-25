00e5e976788f082c41ab79dd21ae709f8a41648b
jfling13 <jfling13@gmail.com>
2023-09-25 17:03:41 -0400

    增加profile路由

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('index/',views.index,name='index'),
     path('get_captcha/',views.get_captcha,name='get_captcha'),
     path('login/',views.login,name='login'),
+    path('profile/',views.profile,name='profile'),
     path('detail/<int:post_id>',views.detail,name='detail'),
     path('comment/<int:post_id>',views.get_comments_by_post,name='get_comments_by_post'),
     path('add_comment/<int:post_id>',views.add_comment,name='add_comment'),
